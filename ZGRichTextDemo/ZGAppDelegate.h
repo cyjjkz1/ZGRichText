e0ec2f3b3bb0d0a25d45f9b711809e2555798708
zhougang <zhougang@gmail.com>
2014-07-16 13:54:03 +0800

    Initial Commit

---
+++ ZGRichTextDemo/ZGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ZGAppDelegate.h
+//  ZGRichTextDemo
+//
+//  Created by mac on 14-7-16.
+//  Copyright (c) 2014年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ZGRichTextDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ZGRichTextDemo
+//
+//  Created by mac on 14-7-16.
+//  Copyright (c) 2014年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ZGAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZGAppDelegate class]));
+    }
+}
